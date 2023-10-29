8fd5655fe7671c93a188234e37f7ec809b4effd0
iyasu2 <iyasuforreal@gmail.com>
2023-10-29 21:03:09 +0300

    s

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,8 @@
 '''
 this is the initialization module
 '''
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from flask import Blueprint
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -9,5 +11,3 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
