1ad80cacf251e4932db226e5c1d34ce9e1610aba
Ab-Ruh <abell.demere@gmail.com>
2023-10-30 19:31:58 +0300

    base

--- models/base_model.py
+++ models/base_model.py
@@ -57,26 +57,23 @@ class BaseModel:
                                          self.__dict__)
 
     def save(self):
-        """Updates the attribute 'updated_at' with the current datetime"""
+        """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.utcnow()
-        if models.storage_t != 'db':
-            models.storage.new(self)
-            models.storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
-    def to_dict(self, save_password=False):
-        """Returns a dictionary containing all keys/values of the instance"""
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
-        if not save_password and "password" in new_dict:
-            del new_dict["password"]
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
         return new_dict
 
     def delete(self):
-        """Delete the current instance from the storage"""
+        """delete the current instance from the storage"""
         models.storage.delete(self)
